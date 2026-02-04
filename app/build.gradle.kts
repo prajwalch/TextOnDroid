344a027d430b8bb3d9eb5322d5e7e3a17900aec6
prajwalch <prajwal.chapagain58@gmail.com>
2026-02-04 18:11:33 +0545

    chore: Add Timber for logging
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.timber)
     implementation(platform(libs.androidx.compose.bom))
 
     testImplementation(libs.junit)
